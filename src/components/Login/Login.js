3befe31daabb51b42156a37e2a8fbd01070806b6
sp simon <cmoxsh@gmail.com>
2021-04-06 17:15:28 +0600

    100% complete pro

---
+++ src/components/Login/Login.js
@@ -0,0 +1,91 @@
+import React, { useContext, useState } from 'react';
+import './Login.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.app(); // if already initialized, use that one
+}
+
+const Login = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+    const history = useHistory();
+    const location = useLocation();
+    let { from } = location.state || { from: { pathname: "/" } };
+
+    const [user, setUser] = useState({
+        isSiggedIn: false,
+        name: '',
+        email: '',
+        password: '',
+        photo: ''
+    });
+
+    // Google Sign In Func
+    const googleSignIn = () => {
+        const googleProvider = new firebase.auth.GoogleAuthProvider();
+        firebase.auth().signInWithPopup(googleProvider)
+            .then((res) => {
+                const { photoURL, displayName, email } = res.user;
+                const siggedInUser = {
+                    isSiggedIn: true,
+                    name: displayName,
+                    email: email,
+                    photo: photoURL
+                }
+                setUser(siggedInUser);
+                setLoggedInUser(siggedInUser);
+                history.replace(from);
+                // console.log(siggedInUser);
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            });
+    }
+
+    // Google Sign Out Func
+    const googleSignOut = () => {
+        firebase.auth().signOut()
+            .then(res => {
+                const siggedOutUser = {
+                    isSiggedIn: false,
+                    name: '',
+                    email: '',
+                    photo: ''
+                }
+                setUser(siggedOutUser);
+                setLoggedInUser(siggedOutUser);
+                // console.log(siggedOutUser);
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
+    return (
+        <div className="App">
+            <h1>Google Authenticatio</h1>
+            <div>
+                {
+                    user.isSiggedIn ? <button onClick={googleSignOut}>Google Sign Out</button> :
+                        <button onClick={googleSignIn}>Google Sign In</button>
+                }
+            </div>
+            <div>
+                <img src={user.photo} alt="" style={{ marginTop: '10px' }} />
+                <h4>Name: {user.name}</h4>
+                <p>Email: {user.email}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCRV5YAL7L0njaJTVg-Pm_T7Tn_Y0qK5Bs",
+    authDomain: "hotel-auth-a5326.firebaseapp.com",
+    projectId: "hotel-auth-a5326",
+    storageBucket: "hotel-auth-a5326.appspot.com",
+    messagingSenderId: "211219262557",
+    appId: "1:211219262557:web:8729dba7bf0b050906b1b4"
+  };
+  export default firebaseConfig;
